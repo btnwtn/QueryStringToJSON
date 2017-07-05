30725cfccf150883dac6ca9b18442d2f484419b9
Brandon Newton <btnwtn@gmail.com>
2017-07-05 15:20:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const _compose = (f, g) => x => f(g(x));
+
+const SearchParams = {
+  of: qs => new URLSearchParams(qs)
+};
+
 const QueryStringToJSON = qs =>
-  Array.from(new URLSearchParams(qs)).reduce((acc, [key, value]) => {
+  Array.from(qs).reduce((acc, [key, value]) => {
     return (acc[key] = value), acc;
   }, {});
 
-export default QueryStringToJSON;
+const qsToJSON = _compose(QueryStringToJSON, SearchParams.of);
+
+export default qsToJSON;
